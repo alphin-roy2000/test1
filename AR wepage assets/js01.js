67d1af0410891d1f02030ec2455e61be03e1f368
Alphin Roy <alphinroy430@gmail.com>
2020-07-09 02:37:25 +0530

    footerand nav arrow test2

--- AR wepage assets/js01.js
+++ AR wepage assets/js01.js
@@ -70,21 +70,21 @@ var x = 'True';
                     el: '.swiper-pagination',
                   },
                 });
-                mybutton = document.getElementById("myBtn");
+                // mybutton = document.getElementById("myBtn");
 
-                // When the user scrolls down 20px from the top of the document, show the button
-                window.onscroll = function() {scrollFunction()};
+                // // When the user scrolls down 20px from the top of the document, show the button
+                // window.onscroll = function() {scrollFunction()};
                 
-                function scrollFunction() {
-                  if (document.body.scrollTop > 1000|| document.documentElement.scrollTop > 1000) {
-                    mybutton.style.display = "block";
-                  } else {
-                    mybutton.style.display = "none";
-                  }
-                }
+                // function scrollFunction() {
+                //   if (document.body.scrollTop > 1000|| document.documentElement.scrollTop > 1000) {
+                //     mybutton.style.display = "block";
+                //   } else {
+                //     mybutton.style.display = "none";
+                //   }
+                // }
                 
-                // When the user clicks on the button, scroll to the top of the document
-                function topFunction() {
-                  document.body.scrollTop = 709; // For Safari
-                  document.documentElement.scrollTop = 709; // For Chrome, Firefox, IE and Opera
-                }
+                // // When the user clicks on the button, scroll to the top of the document
+                // function topFunction() {
+                //   document.body.scrollTop = 709; // For Safari
+                //   document.documentElement.scrollTop = 709; // For Chrome, Firefox, IE and Opera
+                // }
